e5b1da96f9c24d19b25de539e359ce0f6ded9a05
marttha <kapita.mk@gmail.com>
2018-03-11 11:40:47 +0100

    tablice

---
+++ warsztaty/javascript/js/tablice.js
@@ -0,0 +1,72 @@
+var imiona = ['Adam', 'Marta', 'Paweł', 'Michał', 'Kasia', 'Sandra'];
+
+// dodawanie elementów tablicy na końcu
+
+imiona[6] = 'Mikołaj';
+imiona[7] = 'Kinga';
+imiona.push("Kamil", "Karol", "Anna");
+imiona.push("Marcin");
+
+// usuwanie elementów tablicy z końca
+
+imiona.pop();
+imiona.pop();
+
+// dodawanie elementów tablicy na poczatku
+imiona.unshift("Sebastian");
+imiona.unshift("Małgorzata");
+
+//usuwanie elementów z początka  tablicy
+imiona.shift();
+
+//var usunietyElement=imiona.shift;
+//console.log(usunietyElement)
+
+//console.log(imiona);
+
+
+
+
+//sprawdzenie "długości"tablicy
+var iloscElementowTablicy = imiona.length;
+//console.log(iloscElementowTablicy);
+//console.log(imiona);
+
+
+//rozbijanie tablicy na ciąg tekstowy
+var elementyTablicyString = imiona.join(",----,");
+//console.log(elementyTablicyString);
+
+
+////"odwrócenie tablicy"
+//imiona.reverse();
+//console.log(imiona);
+
+//sortowanie tablicy
+//imiona.sort();
+//console.log(imiona);
+
+
+//sprawdzenie czy wartość w tablicy istnieje
+
+//console.log(imiona.indexOf("Sandra"));
+//console.log(imiona.indexOf("Czesław"));
+
+
+imiona.slice(1,2);
+console.log(imiona);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
