63641864b3ebb694727de84f1ce97bcd92670da9
marttha <kapita.mk@gmail.com>
2018-03-11 14:50:02 +0100

    operatoy

---
+++ warsztaty/javascript/js/operatory.js
@@ -0,0 +1,105 @@
+//operatory arytmetyczne
+var liczba1 = 20;
+var liczba2 = 4;
+
+//dodawanie
+var wynki = liczba1 + liczba2;
+
+
+//odejmowanie
+wynki = liczba1 - liczba2;
+
+//mnożenie
+wynki = liczba1 * liczba2;
+
+//dzielenie
+wynki = liczba1 / liczba2;
+
+//dzielenie
+wynki = liczba1 % liczba2;
+
+//console.log(wynki);
+
+//inkrementacja
+liczba2 = liczba2 + 1; //liczba2++
+liczba2 = liczba2 - 1; //liczba2--
+liczba2++;
+liczba2++;
+liczba2--;
+
+console.log(liczba2);
+
+
+
+//operatory przypisania
+var liczba3 = 200;
+var liczba4 = 100;
+var liczba5 = 15;
+var liczba6 = 40;
+var liczba7 = 7;
+
+
+
+liczba3 += 200; //liczba3 = liczba3 +200;
+liczba4 -= 50;   //liczba4 = liczba4 - 50;
+liczba5 *= 2;   //liczba5 = liczba5 * 2;
+liczba6 /= 2;   //liczba6 = liczba6 / 2;
+liczba7 %= 2;   //liczba7 = liczba7 % 2;
+
+
+//console.log(liczba7);
+
+
+//operator porównania
+// var liczba8 = 10;
+// var liczba9 = 10;
+//
+//
+//
+//if( liczba8 !== liczba9){
+//    console.log("Warunek spełniony, czyli true");
+//} else{
+//    console.log("Warunek nie spełniony, czyli false");
+//}
+
+//operatory logiczne
+
+var liczba10 = 20;
+var liczba11 = 4;
+var czyWiosna = "tak";
+
+if(liczba10 < liczba11 || czyWiosna == "tak"){
+    console.log("Warunek spełniony");
+}else{
+    console.log("Warunek nie spełniony");
+    
+    
+    
+}if( !czyWiosna){
+    console.log("Warunek spełniony");
+}else{
+    console.log("Warunek nie spełniony");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
