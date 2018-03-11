90eb8d66b6898cae5522fb245a7f58760fdceeff
marttha <kapita.mk@gmail.com>
2018-03-11 15:21:50 +0100

    instrukcje_warunkowe

---
+++ warsztaty/javascript/js/instrukcje_waruknowe.js
@@ -0,0 +1,60 @@
+////insturkcje if
+//
+var zmienna1 = "Kot";
+var zmienna2 ="Pies";
+//
+//if (zmienna1 != zmienna2){
+//    console.log("Spełniony warunek").
+//}
+
+
+//instrukcja if else
+//
+//if(zmienna1 == zmienn2){
+//    console.log("Spełniamy warunek");
+//} else{
+//    console.log("Nie spełniamy warunku");
+//}
+
+//instrukcja else if
+
+var mojaLiczba =111;
+
+if( mojaLiczba == 5){  
+} else if(mojaLiczba ==5){
+    console.log("Moja liczba to 5");
+}if( mojaLiczba == 12){  
+} else if(mojaLiczba ==12){
+    console.log("Moja liczba to 12");
+}if( mojaLiczba == 65){  
+} else if(mojaLiczba ==65){
+    console.log("Moja liczba to 65");
+}if( mojaLiczba == 90){  
+} else if(mojaLiczba ==90){
+    console.log("Moja liczba to 90");
+} else if ("Moja liczba ma inna wartość");
+
+
+
+
+//instrukcja switch
+
+
+var kolor = "żółty";
+switch(kolor){
+        case "czerwony":
+        console.log("kolor czerwony");
+        break;
+    case "zielony":
+        console.log("Kolor zielony");
+        break;
+    default:
+        console.log("Nie znalazłem dopasowania koloru");
+        
+        
+        
+        
+        
+        
+        
+}
