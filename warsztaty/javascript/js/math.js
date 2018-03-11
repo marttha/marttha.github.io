7b3627cba4b62db94d1fb980696d64a604b74f36
marttha <kapita.mk@gmail.com>
2018-03-11 14:01:38 +0100

    math

---
+++ warsztaty/javascript/js/math.js
@@ -0,0 +1,12 @@
+//właściwości obiektu Math
+console.log(Math.E);
+//liczba PI
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+//metody obiektu Math
+console.log(Math.cos(30));
+console.log(Math.random());
+//potęgowanie
+console.log(Math.pow(2,2));
+
