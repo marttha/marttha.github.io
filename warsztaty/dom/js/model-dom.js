d9b6b1e792bc228b40624d6ef785012b656cd466
marttha <kapita.mk@gmail.com>
2018-03-17 10:27:45 +0100

    model-dom

---
+++ warsztaty/dom/js/model-dom.js
@@ -0,0 +1,15 @@
+//console.log(document);
+//console.dir(document);
+
+//zwroci element body jako obiekt
+//console.dir(document.body);
+
+
+//zwoci element html jako obiekt
+//cosole.dir(document.documentElement);
+
+//Obiekt window
+//console.log(window);
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+console.log(window.document);
