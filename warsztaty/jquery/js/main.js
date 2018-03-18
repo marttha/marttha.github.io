adede7eddfc549cd4b590e1005fd8c3bf53ee16b
marttha <kapita.mk@gmail.com>
2018-03-18 11:37:36 +0100

    document.ready

---
+++ warsztaty/jquery/js/main.js
@@ -0,0 +1,10 @@
+//TRZY SPOSOBY WSTAWIANIA
+//document.ready gwarantuje, żę skrypt wywołają się w momencie gdy cały model DOM jest już załadowany
+//poniżej skrócony zapis document.ready
+
+$(function(){
+//   $(".title").css({color : "red", backgroundColor : "yellow"});
+   $("h2").css({color : "red", backgroundColor : "yellow"});?
+   $("[class = 'titlte']").css({color : "red", backgroundColor : "yellow"});
+    
+});
