977b70d5332753542b19663937a62e9b0be0befe
marttha <kapita.mk@gmail.com>
2018-03-29 19:51:19 +0200

    zmiana w animacji

--- zadania-domowe/jquery/animacja-jquery/js/animacja.js
+++ zadania-domowe/jquery/animacja-jquery/js/animacja.js
@@ -1,14 +1,22 @@
-//function zmianaKoloru = $('div').css({"background-color": "blue";});
-var button = $("#animate");
-$('button').click(function(){
-$('div').animate({
-    backgroundColor:'blue',
-        left: '200px',
-        width: '100px',
-        height: '100px'
-    }, 3000);
-$("div").fadeOut(3000).fadeIn(5000);
-     
-});
+$(document).ready(function () {
+            $('#klikam').click(function() {
+                $('#square').animate({
+                        left: '100px',
+                        width: '100px',
+                        height: '100px'
+                    }, 3000,
+                    function() {
+                        $('#square').animate({
+                                backgroundColor: 'blue',
+                            }, 5000,
+                            function tekst() {
+                                $('.text').show('slow');
+                            });
+                    });
+            });
+        });
+
+
+
 
 
