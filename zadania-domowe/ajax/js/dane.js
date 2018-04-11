68ec9a1e917a4297db74d900d9b4ee34bf1743bf
marttha <kapita.mk@gmail.com>
2018-04-11 19:52:45 +0200

    pobieranie-danych

---
+++ zadania-domowe/ajax/js/dane.js
@@ -0,0 +1,11 @@
+function pobierzDane(event){
+    event.preventDefault();
+    
+    $.getJSON( "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(dane){
+        
+        $('body').append('<p> Imie: ' + dane.imie + "</p>");
+        $('body').append('<p> Nazwisko: ' + dane.nazwisko + "</p>");
+        $('body').append('<p> Zawod: ' + dane.zawod + "</p>");
+        $('body').append('<p> Firma: ' + dane.firma + "</p>");
+    });
+}
