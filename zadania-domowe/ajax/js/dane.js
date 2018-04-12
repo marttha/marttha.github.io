f46aa85377509df4338d015b4cdd7a36a4f018f0
marttha <kapita.mk@gmail.com>
2018-04-12 22:41:00 +0200

    dodanie-div

--- zadania-domowe/ajax/js/dane.js
+++ zadania-domowe/ajax/js/dane.js
@@ -2,7 +2,6 @@ function pobierzDane(event){
     event.preventDefault();
     
     $.getJSON( "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(dane){
-        
         $('body').append('<p> Imie: ' + dane.imie + "</p>");
         $('body').append('<p> Nazwisko: ' + dane.nazwisko + "</p>");
         $('body').append('<p> Zawod: ' + dane.zawod + "</p>");
