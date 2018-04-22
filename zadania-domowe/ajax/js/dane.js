f98f4a7bf423f451516ba3f5b6f99777edffabe8
marttha <kapita.mk@gmail.com>
2018-04-22 11:59:58 +0200

    zamiana body na div

--- zadania-domowe/ajax/js/dane.js
+++ zadania-domowe/ajax/js/dane.js
@@ -2,9 +2,9 @@ function pobierzDane(event){
     event.preventDefault();
     
     $.getJSON( "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(dane){
-        $('body').append('<p> Imie: ' + dane.imie + "</p>");
-        $('body').append('<p> Nazwisko: ' + dane.nazwisko + "</p>");
-        $('body').append('<p> Zawod: ' + dane.zawod + "</p>");
-        $('body').append('<p> Firma: ' + dane.firma + "</p>");
+        $('#dane-programisty').append('<p> Imie: ' + dane.imie + "</p>");
+        $('#dane-programisty').append('<p> Nazwisko: ' + dane.nazwisko + "</p>");
+        $('#dane-programisty').append('<p> Zawod: ' + dane.zawod + "</p>");
+        $('#dane-programisty').append('<p> Firma: ' + dane.firma + "</p>");
     });
 }
